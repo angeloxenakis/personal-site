d40a68ee246d5eeda325fd272f449d5d1df2ba0d
angeloxenakis <angeloxenakis@icloud.com>
2020-06-28 02:20:19 -0500

    completed link tree

--- src/components/linkTree.js
+++ src/components/linkTree.js
@@ -4,54 +4,44 @@ import { linkedIn } from '../assets/linkedin-logo.png'
 export function LinkTree() {
     return (
         <div className="link-tree-container">
-            <h1>Link Tree</h1>
+            <h2>Link Tree</h2>
             <div className="link-tree">
                 <div className="link-tree-left">
                     <div className="link-tree-logo">
                         <div className="linkedin-panel"></div>
-                        <h5>Linked In</h5>
+                        <h5><a href="https://www.linkedin.com/in/angeloxenakis" target="_blank">Linked In</a></h5>
                     </div>
                     <div className="link-tree-logo">
                         <div className="github-panel"></div>
-                        <h5>GitHub</h5>
+                        <h5><a href="https://github.com/angeloxenakis" target="_blank">GitHub</a></h5>
+                    </div>
+                    <div className="link-tree-logo">
+                        <div className="medium-panel"></div>
+                        <h5><a href="https://medium.com/@angeloxenakis" target="_blank">Medium</a></h5>
+                    </div>
+                </div>
+                <div className="link-tree-left">
+                    <div className="link-tree-logo">
+                        <div className="behance-panel"></div>
+                        <h5><a href="https://dribbble.com/AngeloTheThird" target="_blank">Behance</a></h5>
                     </div>
                     <div className="link-tree-logo">
                         <div className="dribbble-panel"></div>
-                        <h5>Dribbble</h5>
+                        <h5><a href="https://dribbble.com/AngeloTheThird" target="_blank">Dribbble</a></h5>
                     </div>
+                </div>
+                <div className="link-tree-right">
                     <div className="link-tree-logo">
-                        <div className="portfolio-panel"></div>
-                        <h5>Portfolio</h5>
+                        <div className="youtube-panel"></div>
+                        <h5><a href="https://www.youtube.com/watch?v=99Fql6xGipU" target="_blank">YouTube</a></h5>
                     </div>
                     <div className="link-tree-logo">
                         <div className="spotify-panel"></div>
-                        <h5>Spotify</h5>
-                    </div>
-                    <div className="link-tree-logo">
-                        <div className="soundcloud-panel"></div>
-                        <h5>SoundCloud</h5>
-                    </div>  
-                </div>
-                <div className="link-tree-right">
-                    <div className="link-tree-logo-right">
-                        <div className="medium-panel"></div>
-                        <h5>Medium</h5>
-                    </div>
-                    <div className="link-tree-logo">
-                        <div className="portfolio-panel"></div>
-                        <h5>Portfolio</h5>
-                    </div>
-                    <div className="link-tree-logo">
-                        <div className="behance-panel"></div>
-                        <h5>Behance</h5>
-                    </div>
-                    <div className="link-tree-logo">
-                        <div className="youtube-panel"></div>
-                        <h5>YouTube</h5>
-                    </div>
+                        <h5><a href="https://open.spotify.com/artist/4iFCXVjW4KECpZ4tbY0eon" target="_blank">Spotify</a></h5>
+                    </div> 
                     <div className="link-tree-logo">
                         <div className="applemusic-panel"></div>
-                        <h5>Apple Music</h5>
+                        <h5><a href="https://music.apple.com/us/artist/angelo-the-third/1457300377" target="_blank">Apple Music</a></h5>
                     </div>  
                 </div>
             </div>
